f5f9b54a3560ff8925a4d42e6823e744d361cee6
roshan.chhipavadiya <roshan.chhipavadiya@rentechdigital.com>
2023-04-28 14:48:34 +0530

    App listen and script changes

--- connnection/connection.js
+++ connnection/connection.js
@@ -10,14 +10,14 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log('Database connected successfully.. ');
 });
 
-const PORT = process.env.PORT ?? 4000;
-app.listen(PORT, (err) => { 
-    if (err) {
-        console.log(' Error is %s', err);
-    } else { 
-        console.log(' API LISTENING on %s', PORT);   
-    }
-});
+// const PORT = process.env.PORT ?? 4000;
+// app.listen(PORT, (err) => { 
+//     if (err) {
+//         console.log(' Error is %s', err);
+//     } else { 
+//         console.log(' API LISTENING on %s', PORT);   
+//     }
+// });
 
 module.exports = {mongoose, app};
 // exports.module = app;
