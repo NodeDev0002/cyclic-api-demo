5b4bbf1e07941e501a01ea0f0579893e6db7dc98
roshan.chhipavadiya <roshan.chhipavadiya@rentechdigital.com>
2023-05-01 18:06:45 +0530

    Comment Count Added

--- modules/comment/apis/comment.js
+++ modules/comment/apis/comment.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const CommentModel = require('../models/comment_model');
+const PostModel = require('../../post/models/post_model');
 const SubCommentModel = require('../models/sub_comment_model');
 
 const giveResponse = require('../../../utils/giveResponse');
@@ -19,6 +20,8 @@ async function addComment(req, res, next) {
                 commentTime: commentTime,
             }
         );
+        const commentCount = await CommentModel.find({ postId: postId });
+        await PostModel.findByIdAndUpdate({ _id: postId }, { postCommentCount: commentCount.length });
         var comments = await comment.save();
         return giveResponse(req, res, true, 200, "Comment Done.. ", comments);        
     } catch (err) {
